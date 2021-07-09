c1a14945ce1c070b7d56c9baa02573f65e096749
Christopher Song <sonchr@amazon.com>
2021-07-09 12:43:21 -0500

    Add starter files from Spring Initializr

---
+++ src/main/java/com/example/NbaQuickStats/NbaQuickStatsApplication.java
@@ -0,0 +1,13 @@
+package com.example.NbaQuickStats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbaQuickStatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NbaQuickStatsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/NbaQuickStats/NbaQuickStatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NbaQuickStats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbaQuickStatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
